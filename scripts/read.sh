79d5bf75a6bd0955804ad5366f208671223d54ef
Wouter Hemeryck <wouter.hemeryck@delawareconsulting.com>
2023-04-30 14:02:08 +0200

    Script to export HA config

---
+++ scripts/read.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+HOST=$1
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+USER="root"
+
+if [ -z "${HOST}" ]; then
+    echo '[ERROR] HOST is required, run read.sh host' >&2
+    exit 1
+fi
+
+echo "-------------------------------------------------------"
+echo "[INFO] CLEAR EXISTING LOCAL CONFIG"
+echo "-------------------------------------------------------"
+cd $DIR
+rm -rf ../config/ || true
+mkdir ../config/
+
+echo "-------------------------------------------------------"
+echo "[INFO] COPY FROM ${HOST}"
+echo "-------------------------------------------------------"
+cd $DIR
+scp -r $USER@$HOST:~/config ../
+
+echo "-------------------------------------------------------"
+echo "[INFO] REMOVING UNNEEDED FILES"
+echo "-------------------------------------------------------"
+cd $DIR
+echo "Deleting files in gitignore"
+git clean -fX
